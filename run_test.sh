ebc77d2fc7b4bb72e57cf50779962ca2731efbbf
Manoel Carvalho <manoelhc@gmail.com>
2016-07-15 08:43:54 -0300

    Add a sleep

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-python ./chef-tag-api.py >/dev/null &
+echo "################################################"
+python3 ./chef-tag-api.py >/dev/null &
+sleep 5
 restafari -s localhost -P 9999 api_test/*
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+ps -ef | grep chef-tag-api | grep -v grep
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 PID=$(ps -ef | grep chef-tag-api | grep -v grep | awk '{print $2}')
 [[ -n ${PID} ]] && kill ${PID}
+echo "################################################"
